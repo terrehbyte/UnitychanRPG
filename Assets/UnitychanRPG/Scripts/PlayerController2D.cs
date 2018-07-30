fd8ee64085910d282c083d280e52791442ad3c28
Terry Nguyen <terreh@terrehbyte.com>
2018-07-30 02:44:49 -0700

    Stub out tech demo level

---
+++ Assets/UnitychanRPG/Scripts/PlayerController2D.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    [SerializeField]
+    [HideInInspector]
+    private Rigidbody2D rbody;
+
+    [SerializeField]
+    [HideInInspector]
+    private Collider2D coll2D;
+
+    // Last polled player input vector
+    [Header("Player Movement")]
+    private Vector2 lastPlayerInput;
+
+    [SerializeField]
+    private float baseMoveSpeed = 5.0f;
+    [SerializeField]
+    private float dashMultiplier = 2.0f;
+
+    [SerializeField]
+    private int maxTraceHits = 32;
+    private RaycastHit2D[] lastTraceHitResults;
+    private int lastTraceHitCount = 0;
+
+    [SerializeField]
+    private LayerMask traceLayer;
+
+    PlayerController2D()
+    {
+        lastTraceHitResults = new RaycastHit2D[maxTraceHits];
+    }
+
+    private void Update()
+    {
+        lastPlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"),
+                                      Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        float targetMoveSpeed = baseMoveSpeed * (Input.GetButton("Sprint") ? dashMultiplier : 1.0f);
+        /*
+        Vector2 targetPlayerDelta = lastPlayerInput * targetMoveSpeed * Time.deltaTime;
+        lastTraceHitCount = coll2D.Cast(targetPlayerDelta, lastTraceHitResults);
+
+        float nearestResultDistance = float.MaxValue;
+        int nearestResultIndex = -1;
+        for(int i = 0; i < lastTraceHitCount; ++i)
+        {
+            if(lastTraceHitResults[i].distance < nearestResultDistance)
+            {
+                nearestResultDistance = lastTraceHitResults[i].distance;
+                nearestResultIndex = i;
+            }
+        }
+
+        if(nearestResultIndex != -1)
+        {
+            Vector2 worldHitPoint = lastTraceHitResults[nearestResultIndex].point;
+        }
+        */
+
+        rbody.velocity = lastPlayerInput * targetMoveSpeed;
+    }
+
+    void Reset()
+    {
+        traceLayer = LayerMask.GetMask("Player");
+        rbody = GetComponent<Rigidbody2D>();
+        coll2D = GetComponent<Collider2D>();
+    }
+}
---
+++ Assets/UnitychanRPG/Scripts/TileCollisionRouter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileCollisionRouter : MonoBehaviour
+{
+    [SerializeField]
+    [HideInInspector]
+    protected Tilemap tilemap;
+
+    [SerializeField]
+    protected int maxContactPoints = 32;
+    protected ContactPoint2D[] lastCollisionContacts;
+
+    // Offset applied to contact point to determine position of collided tile
+    [SerializeField]
+    protected float contactResolutionOffset = 0.01f;
+
+    TileCollisionRouter()
+    {
+        lastCollisionContacts = new ContactPoint2D[maxContactPoints];
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        RouteCollision(ContactTiming.Start, collision);
+    }
+
+    protected virtual void OnCollisionStay2D(Collision2D collision)
+    {
+        RouteCollision(ContactTiming.Stay, collision);
+    }
+
+    protected virtual void OnCollisionExit2D(Collision2D collision)
+    {
+        RouteCollision(ContactTiming.Exit, collision);
+    }
+
+    protected virtual void RouteCollision(ContactTiming timing, Collision2D collision)
+    {
+        // What happens if the array is too small?
+
+        int contacts = collision.contactCount;
+        collision.GetContacts(lastCollisionContacts);
+
+        foreach(var contact in lastCollisionContacts)
+        {
+            Vector2 roughTilePosition = new Vector2(contact.point.x - 0.01f * contact.normal.x,
+                                                    contact.point.y - 0.01f * contact.normal.y);
+
+            //tilemap.WorldToCell(roughTilePosition);
+        }
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        RouteTrigger(ContactTiming.Start, other);
+    }
+
+    protected virtual void OnTriggerStay2D(Collider2D other)
+    {
+        RouteTrigger(ContactTiming.Stay, other);
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D other)
+    {
+        RouteTrigger(ContactTiming.Exit, other);
+    }
+
+    protected virtual void RouteTrigger(ContactTiming timing, Collider2D other)
+    {
+        // Is this actually gonna get used...?
+        other.GetComponent<ITileCollidable>().OnTrigger(timing, other);
+    }
+
+    void OnValidate()
+    {
+        //tilemap = tilemap == null ? GetComponent<Tilemap>() : tilemap;
+    }
+}
