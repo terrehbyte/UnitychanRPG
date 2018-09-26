b708dd5d9c9157def44c851a542f12dddc3235ee
Terry Nguyen <terreh@terrehbyte.com>
2018-09-25 21:14:24 -0700

    Separate locomotion away from controller

--- Assets/UnitychanRPG/Scripts/PlayerController2D.cs
+++ Assets/UnitychanRPG/Scripts/PlayerController2D.cs
@@ -12,31 +12,25 @@ public class PlayerController2D : MonoBehaviour
     [HideInInspector]
     private Collider2D coll2D;
 
-    // Last polled player input vector
-    [Header("Player Movement")]
-    private Vector2 lastPlayerInput;
+    [SerializeField]
+    [HideInInspector]
+    private PlayerLocoTopDown2D locomotion;
 
-    [SerializeField]
-    private float baseMoveSpeed = 5.0f;
-    [SerializeField]
-    private float dashMultiplier = 2.0f;
+    // Last polled player input vector
+    private Vector2 lastPlayerInput;
 
     private void Update()
     {
         lastPlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"),
                                       Input.GetAxisRaw("Vertical"));
+
+        locomotion.SetMovementInput(lastPlayerInput);
     }
 
-    private void FixedUpdate()
-    {
-        float targetMoveSpeed = baseMoveSpeed * (Input.GetButton("Sprint") ? dashMultiplier : 1.0f);
-
-        rbody.velocity = lastPlayerInput.normalized * targetMoveSpeed;
-    }
-
-    void Reset()
+    private void Reset()
     {
         rbody = rbody != null ? rbody : GetComponent<Rigidbody2D>();
         coll2D = coll2D != null ? coll2D : GetComponent<Collider2D>();
+        locomotion = locomotion != null ? locomotion : GetComponent<PlayerLocoTopDown2D>();
     }
 }
---
+++ Assets/UnitychanRPG/Scripts/PlayerLocoTopDown2D.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocoTopDown2D : MonoBehaviour
+{
+    [SerializeField]
+    [HideInInspector]
+    private Rigidbody2D rbody;
+
+    [SerializeField]
+    [HideInInspector]
+    private Collider2D coll2D;
+
+    // Last polled player input vector
+    [Header("Player Movement")]
+    private Vector2 lastMovementInput;
+
+    [SerializeField]
+    private float baseMoveSpeed = 5.0f;
+    [SerializeField]
+    private float dashMultiplier = 2.0f;
+
+    public void SetMovementInput(Vector2 input)
+    {
+        lastMovementInput = input;
+    }
+
+    private void FixedUpdate()
+    {
+        float targetMoveSpeed = baseMoveSpeed * (Input.GetButton("Sprint") ? dashMultiplier : 1.0f);
+
+        rbody.velocity = lastMovementInput.normalized * targetMoveSpeed;
+    }
+
+    private void Reset()
+    {
+        rbody = rbody != null ? rbody : GetComponent<Rigidbody2D>();
+        coll2D = coll2D != null ? coll2D : GetComponent<Collider2D>();
+    }
+}
