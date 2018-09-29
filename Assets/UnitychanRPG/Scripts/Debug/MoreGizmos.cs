50637f62716ef513bcbefea17d75f76a246f91e0
Terry Nguyen <terreh@terrehbyte.com>
2018-09-29 14:48:33 -0700

    Add 2D square and circle gizmos

--- Assets/UnitychanRPG/Scripts/Debug/MoreGizmos.cs
+++ Assets/UnitychanRPG/Scripts/Debug/MoreGizmos.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 // TODO: Optimize this out for builds where Gizmos can't be seen anyway
 // TODO: Print stack of strings
 //   - this should accept a function used to determine a value to print to screen
+// TODO: support execution during edit-time
+
+// LONG-TODO: Support execution at run-time https://docs.unity3d.com/ScriptReference/GL.html
+//            I don't like the idea of modifying script execution order, so this should be
+//            added to a custom update loop
 
 // Retained mode wrapper for Unity's Gizmos class
 public class MoreGizmos : MonoBehaviour
 {
+    // TODO: consider exposing GizmoDraw for custom behavior from end-user
+
     abstract class GizmoDraw
     {
         public Vector3 position;
@@ -26,6 +33,10 @@ public class MoreGizmos : MonoBehaviour
             }
         }
 
+        public virtual void BeforeDraw()
+        {
+            Gizmos.color = color;
+        }
         public abstract void Draw();
     }
     class GizmoSphere : GizmoDraw
@@ -41,7 +52,6 @@ public class MoreGizmos : MonoBehaviour
 
         public override void Draw()
         {
-            Gizmos.color = color;
             Gizmos.DrawSphere(position, radius);
         }
     }
@@ -57,8 +67,85 @@ public class MoreGizmos : MonoBehaviour
         public Vector3 size;
         public override void Draw()
         {
-            Gizmos.color = color;
             Gizmos.DrawCube(position, size);
+        }
+    }
+    class GizmoCircle : GizmoDraw
+    {
+        public Vector3 normal;
+        public float radius;
+        public int sides;
+
+        public GizmoCircle(Vector3 pos, Vector3 norm, float rad, int sideCount, Color col)
+        {
+            Debug.Assert(sideCount > 2);
+
+            position = pos;
+            normal = norm;
+            radius = rad;
+            sides = sideCount;
+            color = col;
+        }
+
+        public override void Draw()
+        {
+            float step = 2 * Mathf.PI / (float)sides;
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, normal);
+
+            for(int i = 0; i < sides; ++i)
+            {
+                int startIdx = i;
+                int endIdx = (i+1) % (sides);
+
+                Vector3 legStart = rot * new Vector3(radius * Mathf.Cos(step * startIdx), radius * Mathf.Sin(step * startIdx), 0.0f);
+                Vector3 legEnd = rot * new Vector3(radius * Mathf.Cos(step * endIdx),   radius * Mathf.Sin(step * endIdx), 0.0f);
+
+                Gizmos.DrawLine(position + legStart, position + legEnd);
+            }
+
+            return; // remove below when done
+        }
+    }
+    class GizmoSquare : GizmoDraw
+    {
+        public float degrees;
+        public Vector3 normal;
+        public Vector2 size;
+
+        public GizmoSquare(Vector3 center, Vector3 norm, Vector2 extents, float deg, Color col)
+        {
+            position = center;
+            normal = norm;
+            size = extents;
+            color = col;
+            degrees = deg;
+        }
+
+        public override void Draw()
+        {
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, normal);
+
+            Vector2 halfExt = size / 2;
+
+            Quaternion localRot = Quaternion.AngleAxis(degrees, Vector3.forward);
+
+            Vector3[] verts =
+            {
+                localRot * new Vector3(-halfExt.x,  halfExt.y, 0), // top-left
+                localRot * new Vector3( halfExt.x,  halfExt.y, 0), // top-right
+                localRot * new Vector3( halfExt.x, -halfExt.y, 0), // bottom-right
+                localRot * new Vector3(-halfExt.x, -halfExt.y, 0), // bottom-left
+            };
+
+            for(int i = 0; i < verts.Length; ++i)
+            {
+                int endIdx = (i+1) % (verts.Length);
+
+                Vector3 legStart = (position + rot * verts[i]);
+                Vector3 legEnd = (position + rot * verts[endIdx]);
+
+                Gizmos.DrawLine(legStart, legEnd);
+            }
         }
     }
 
@@ -110,6 +197,18 @@ public class MoreGizmos : MonoBehaviour
     }
 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void DrawCircle(Vector3 center, Vector3 normal, float radius = 1.0f, int sides = 9, Color color = default(Color))
+    {
+        instance.gizmos.Add(new GizmoCircle( center, normal, radius, sides, color ));
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void DrawSquare(Vector3 center, Vector3 normal, Vector2 size, float degrees = 0.0f, Color color = default(Color))
+    {
+        instance.gizmos.Add(new GizmoSquare( center, normal, size, degrees, color ));
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
     private void Awake()
     {
         _instance = (_instance == null ? this : _instance);
@@ -122,6 +221,7 @@ public class MoreGizmos : MonoBehaviour
 
         for(int i = 0; i < gizmos.Count; ++i)
         {
+            gizmos[i].BeforeDraw();
             gizmos[i].Draw();
         }
 
